8457e152b43a1778b63bba1265934f2db096fa60
RobertAb19 <95838052+RobertAb19@users.noreply.github.com>
2025-06-11 22:03:24 -0500

    Update wbotMessageListener.ts

--- backend/src/services/WbotServices/wbotMessageListener.ts
+++ backend/src/services/WbotServices/wbotMessageListener.ts
@@ -2431,12 +2431,14 @@ const filterMessages = (msg: WAMessage): boolean => {
       WAMessageStubType.E2E_DEVICE_CHANGED,
       WAMessageStubType.E2E_IDENTITY_CHANGED,
       WAMessageStubType.CIPHERTEXT
-    ].includes(msg.messageStubType as WAMessageStubType)
-  )
+    ].includes(msg.messageStubType)
+  ) {
     return false;
+  }
 
   return true;
 };
+
 
 const wbotMessageListener = async (wbot: Session, companyId: number): Promise<void> => {
   try {
